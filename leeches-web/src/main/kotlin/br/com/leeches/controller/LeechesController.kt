52310aa98a12c0a17784894257f7b40dd00cea18
Danilo Paiva <danilopaivasilva@gmail.com>
2018-05-29 10:49:11 -0300

    leeches-api module created

--- leeches-web/src/main/kotlin/br/com/leeches/controller/LeechesController.kt
+++ leeches-web/src/main/kotlin/br/com/leeches/controller/LeechesController.kt
@@ -1,44 +1,27 @@
 package br.com.leeches.controller
 
+import br.com.leeches.api.LeechesApi
 import br.com.leeches.representation.LeechRepresentation
 import br.com.leeches.request.LeechRequest
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 import java.util.*
 import javax.validation.Valid
 
 @RestController
-@RequestMapping("/leeches")
-class LeechesController {
+class LeechesController : LeechesApi {
 
     companion object {
         private val leeches = hashMapOf<String, LeechRepresentation>()
     }
 
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @ResponseBody
-    @PostMapping(produces = [(MediaType.APPLICATION_JSON_VALUE)])
-    fun save(@RequestBody @Valid request: LeechRequest): LeechRepresentation {
+    override fun save(@RequestBody @Valid request: LeechRequest): LeechRepresentation {
         return saveLeech(request)
     }
 
-    /**
-     * @param id ID of the leech.
-     */
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    @GetMapping("/{id}", produces = [(MediaType.APPLICATION_JSON_VALUE)])
-    fun find(@PathVariable("id") id: String): LeechRepresentation {
+    override fun find(@PathVariable("id") id: String): LeechRepresentation {
         return Optional.ofNullable(leeches[id])
                 .orElseThrow {
                     Exception()
